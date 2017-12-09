675d75b9c193592cc37da39cf65fd61cd3e11f01
robertny <robertny@ondemand.co.ke>
2017-12-09 12:57:34 +0300

    update svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+from sklearn.metrics import accuracy_score
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+clf = svm.SVC(kernel='rbf', C=10000.0, gamma='auto')
+t0 = time()
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+predx = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+# print "prediction for 10th elem: ", predx[10], " 26th elem: ", predx[26], "50th elem: ", predx[50]
+count = 0
+for label in predx:
+    if(label == 1):
+        count = count+1
+print "chris has ", count, "events"
+accuracy = accuracy_score(labels_test, predx)
+print(accuracy)
